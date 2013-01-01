92e986fe2ee070ad8e65e65fc8e53444090e1249
William Blankenship <william.jblankenship@gmail.com>
2013-01-01 16:38:13 -0600

    Matrix-fyed the layout.

--- gen/blankenship/william/NewYearsResolution/R.java
+++ gen/blankenship/william/NewYearsResolution/R.java
@@ -10,40 +10,50 @@ package blankenship.william.NewYearsResolution;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int Black=0x7f040001;
+        public static final int CalendarNonMonthGreen=0x7f040002;
+        public static final int CalendarWeekBlack=0x7f040003;
+        public static final int Green=0x7f040000;
+    }
+    public static final class dimen {
+        public static final int buttonMargin=0x7f050000;
+    }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f070000;
-        public static final int calText=0x7f070008;
-        public static final int calendarView=0x7f070001;
-        public static final int content=0x7f070006;
-        public static final int handle=0x7f070007;
-        public static final int menu_settings=0x7f070009;
-        public static final int pauseTimer=0x7f070004;
-        public static final int slidingDrawer1=0x7f070005;
-        public static final int startTimer=0x7f070003;
-        public static final int timer=0x7f070002;
+        public static final int LinearLayout1=0x7f090000;
+        public static final int calText=0x7f090002;
+        public static final int calendarView=0x7f090001;
+        public static final int content=0x7f090007;
+        public static final int handle=0x7f090008;
+        public static final int menu_settings=0x7f090009;
+        public static final int pauseTimer=0x7f090005;
+        public static final int slidingDrawer1=0x7f090006;
+        public static final int startTimer=0x7f090004;
+        public static final int timer=0x7f090003;
     }
     public static final class layout {
         public static final int activity_calendar=0x7f030000;
         public static final int activity_timer=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_calendar=0x7f060000;
-        public static final int activity_timer=0x7f060001;
+        public static final int activity_calendar=0x7f080000;
+        public static final int activity_timer=0x7f080001;
     }
     public static final class string {
-        public static final int SwitchToCalendar=0x7f040004;
-        public static final int SwitchToTheTimer=0x7f040005;
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_calendar=0x7f040003;
-        public static final int title_activity_timer=0x7f040006;
+        public static final int SwitchToCalendar=0x7f060004;
+        public static final int SwitchToTheTimer=0x7f060005;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_settings=0x7f060002;
+        public static final int title_activity_calendar=0x7f060003;
+        public static final int title_activity_timer=0x7f060006;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f070000;
+        public static final int CalendarMonth=0x7f070001;
     }
 }
